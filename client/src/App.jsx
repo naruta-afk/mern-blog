c03d4fe4419ac86750a55b5bb1fa7c319fbd9b35
naruta-afk <affa.15md@gmail.com>
2025-01-08 13:57:41 +0100

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Signin from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import About from "./pages/About"
 
 export default function App() {
   return (
-    <dh1 className="text-3xl text-red-500">
-      
-      App
-    </dh1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/About " element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
- 
